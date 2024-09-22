6933380d5c11319806fe5f40cf80ba39dfe6df78
Juichilee <juichi.lee.0612@gmail.com>
2024-09-21 20:14:07 -0700

    Added Basic Melee Attack to Damage Enemies

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Status : MonoBehaviour
+{
+
+    public float maxHealth = 2f;
+    public float currHealth;
+
+    // public UnityEvent onDefeated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currHealth -= damage;
+        if (currHealth <= 0) {
+            this.gameObject.SetActive(false);
+            OnDefeated();
+        }
+    }
+
+    public void OnDefeated()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
