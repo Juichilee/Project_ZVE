8d835d0e5c1dd023bb6211aa98db38424f5fe7d8
Juan Benjamin <juanbenjamin2000@gmail.com>
2024-10-01 18:14:34 -0700

    Pause Menu Prefab created.

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject settingsMenu;
+    public GameObject pauseMenu;
+    private static bool isPaused;
+
+    public static bool GetIsPaused()
+    {
+        return isPaused;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (isPaused)
+                Unpause();
+            else
+                Pause();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isPaused = true;
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game would quit now");
+    }
+
+    
+}
