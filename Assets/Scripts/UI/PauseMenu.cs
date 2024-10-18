7c6f3135103432965a8b900efd4444e6d3da17af
Juan Benjamin <juanbenjamin2000@gmail.com>
2024-10-18 16:52:46 -0700

    you died screen

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -19,7 +19,7 @@ public class PauseMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Unpause();
     }
 
     private void Update()
