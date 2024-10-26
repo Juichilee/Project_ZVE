e0771dda524c8178b350c4fa59d1944890c1b657
Juan Benjamin <juanbenjamin2000@gmail.com>
2024-10-25 21:35:34 -0700

    Enemy Zombie V2

--- Assets/Scripts/Weapons/EnemyDamageable.cs
+++ Assets/Scripts/Weapons/EnemyDamageable.cs
@@ -7,13 +7,22 @@ public class EnemyDamageable : BasicDamageable
     public Material origMaterial;
     public Material onDamageMaterial;
     public MeshRenderer meshRenderer;
+    public RagdollOnDeath ragdollOnDeath;
+    public AudioClip deathSound;
+    private AudioSource audioSource;
+    public DamageData damage;
+    public float AttackDamage { get; private set; }
+    public float AttackSpeed { get; private set; }
+    public bool CanAttack { get; private set; }
+
+
     void Awake()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
-        origMaterial = meshRenderer.material;
+        //meshRenderer = GetComponent<MeshRenderer>();
+        //origMaterial = meshRenderer.material;
     }
 
-        public override void OnDamage(DamageData damageData)
+    public override void OnDamage(DamageData damageData)
     {
         if (IsInvincible || !IsAlive) return;
         CurrentHealth -= damageData.BaseDamage;
@@ -25,10 +34,10 @@ public class EnemyDamageable : BasicDamageable
         if (damageData.BleedDamage > 0) BleedHealth -= damageData.BleedDamage;
 
         // Visualize taking damage
-        meshRenderer.material = onDamageMaterial;
+        //if(meshRenderer != null) meshRenderer.material = onDamageMaterial;
 
         StartCoroutine(InvincibilityFrames());
-
+        Debug.Log(CurrentHealth);
         if (CurrentHealth <= 0)
         {
             Die();
@@ -36,14 +45,35 @@ public class EnemyDamageable : BasicDamageable
     }
 
     public void Update(){
-        if (!IsInvincible){
-            meshRenderer.material = origMaterial;
-        }
+        //if (!IsInvincible){
+        //    meshRenderer.material = origMaterial;
+        //}
     }
 
     public override void Die()
     {
-        Debug.Log($"{gameObject.name} has died.");
-        // Handle object destruction or death animations
+        Debug.Log("dieing now");
+        ragdollOnDeath.EnableRagdoll();
+        if (deathSound != null)
+        {
+            audioSource.PlayOneShot(deathSound);
+        }
+
     }
+
+    public void OnDefeated()
+    {
+        ragdollOnDeath.EnableRagdoll();
+        if (deathSound != null)
+        {
+            audioSource.PlayOneShot(deathSound);
+        }
+
+    }
+
+    public void DealDamage(PlayerStatus receiverStatus)
+    {
+        receiverStatus.OnDamage(damage);
+    }
+
 }
