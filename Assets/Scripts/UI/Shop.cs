93a7e8682a7485e035a7d51e0f8dacac365649e7
Juan Benjamin <juanbenjamin2000@gmail.com>
2024-10-25 17:46:44 -0700

    Upgrade Shop Menu

--- Assets/Scripts/UI/Shop.cs
+++ Assets/Scripts/UI/Shop.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
     public static int HealthCost = 2;
     public static int SpeedCost = 2;
     public static int StrengthCost = 2;
+    public static int UnstableCost = 2;
     public int costIncrease = 2;
     Status playerStatus;
     public TMP_Text HealthText;
     public TMP_Text SpeedText;
     public TMP_Text StrengthText;
+    public TMP_Text UnstableText;
+    public Slider MonsterLevel;
 
     // Start is called before the first frame update
     void Start()
     {
         playerStatus = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Status>();
+        MonsterLevel.maxValue = 5;
+        MonsterLevel.minValue = 0;
     }
 
     // Update is called once per frame
@@ -26,6 +32,8 @@ public class Shop : MonoBehaviour
         HealthText.text = HealthCost.ToString();
         SpeedText.text = SpeedCost.ToString();
         StrengthText.text = StrengthCost.ToString();
+        UnstableText.text = UnstableCost.ToString();
+        MonsterLevel.value = playerStatus.monsterPoints;
     }
 
     public void Health()
@@ -57,4 +65,40 @@ public class Shop : MonoBehaviour
             StrengthCost += costIncrease;
         }
     }
+
+    public void UnstableMutation()
+    {
+        if (DNA.GetPoints() >= UnstableCost)
+        {
+            DNA.Addpoints(-1 * UnstableCost);
+            randomUpgrade();
+            randomUpgrade();
+            UnstableCost += costIncrease;
+            playerStatus.monsterPoints += 1;
+        }
+    }
+
+    void randomUpgrade()
+    {
+        int randomvalue = Random.Range(0, 2);
+        switch (randomvalue)
+        {
+            case 0:
+                playerStatus.strengthUpgrade += 1;
+                StrengthCost += costIncrease;
+                break;
+            case 1:
+                playerStatus.speedUpgrade += 1;
+                SpeedCost += costIncrease;
+                break;
+            case 2:
+                playerStatus.maxHealth += 20;
+                HealthCost += costIncrease;
+                break;
+            default:
+                break;
+        }
+    }
 }
+
+
