fe1683f7feac56ad6e814d0f7e8d9f1775c14f64
Juichilee <juichi.lee.0612@gmail.com>
2024-11-10 17:14:39 -0800

    Fixed bullets and melee hitboxes colliding with trigger layer

--- Assets/Scripts/Weapons/DamageObject.cs
+++ Assets/Scripts/Weapons/DamageObject.cs
@@ -4,6 +4,7 @@ public class DamageObject : MonoBehaviour
 {
     public Weapon weaponSource; // Weapon ref used by Damageable objects to calculate OnDamage
     public Transform attacker;
+    public LayerMask damageLayer;
 
     public void SetDamageSource(Weapon weapon)
     {
@@ -13,8 +14,15 @@ public class DamageObject : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+
+        // Convert the collided layer to a bitmask
+        int objLayerMask = 1 << other.gameObject.layer;
+    
+        // Perform bitwise AND between the LayerMask and the object's layer mask
+        bool isDamageable = (damageLayer.value & objLayerMask) != 0;
+
         // Ignore the weapon holder and the weapon itself
-        if (weaponSource == null || other.gameObject.transform.root == attacker)
+        if (weaponSource == null || other.gameObject.transform.root == attacker || !isDamageable)
         {
             return;
         }
