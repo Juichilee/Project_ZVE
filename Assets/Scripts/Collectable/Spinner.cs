50046c924c7cbadc44f55a854451d270e2f630b1
Juan Benjamin <juanbenjamin2000@gmail.com>
2024-11-16 14:15:38 -0800

    More obvious that weapons exist

--- Assets/Scripts/Collectable/Spinner.cs
+++ Assets/Scripts/Collectable/Spinner.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Float : MonoBehaviour
+public class Spinner : MonoBehaviour
 {
+    public int type = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class Float : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Vector3.up);
+        if (type == 0)
+            this.transform.Rotate(Vector3.right);
+        else
+            this.transform.Rotate(Vector3.up);
     }
 }
